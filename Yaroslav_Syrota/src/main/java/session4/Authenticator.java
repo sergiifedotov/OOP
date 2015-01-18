78336f5d42a45c9c893191e2a4d20a50542f3e85
Yaroslav_Syrota <caterpiller@ex.ua>
2015-01-18 17:06:18 +0200

    new Authenticator

---
+++ Yaroslav_Syrota/src/main/java/session4/Authenticator.java
@@ -0,0 +1,24 @@
+package session4;
+
+import session2.*;
+
+import java.util.HashMap;
+
+/**
+ * Created by Admin on 18.01.2015.
+ */
+public class Authenticator {
+    private HashMap<String, String> users = new HashMap<String, String>();
+
+    private static final Authenticator AUTHENTICATOR = new Authenticator();
+
+    public Authenticator(){}
+
+
+
+
+    public boolean auth(String l, String p) {
+        if(users.containsKey(l) && users.containsValue(p)) return true;
+        else return false;
+    }
+}
