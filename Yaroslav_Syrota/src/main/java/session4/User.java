cb8c90cb0e5632ec2ae625aa3e8a952fc39e397e
Yaroslav_Syrota <caterpiller@ex.ua>
2015-01-18 17:47:27 +0200

    obratniy sort

---
+++ Yaroslav_Syrota/src/main/java/session4/User.java
@@ -0,0 +1,61 @@
+package session4;
+
+/**
+ * Created by Yaroslav_Syrota on 18.01.2015.
+ */
+public class User {
+    private String login;
+    private String pass;
+
+    public User() {
+    }
+
+    public User(String login, String pass) {
+        this.login = login;
+        this.pass = pass;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        User user = (User) obj;
+
+        if (!login.equals(user.login)) {
+            return false;
+        }
+        if (!pass.equals(user.pass)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = login.hashCode();
+        result = 31 * result + pass.hashCode();
+        return result;
+    }
+}
