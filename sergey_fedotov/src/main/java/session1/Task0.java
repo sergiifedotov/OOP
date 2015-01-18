cdb74bfc1975d7e8f5e695eefe963a63c8634c73
sergey_fedotov <sergey_fedotov@sergey_fedotov>
2015-01-18 16:08:26 +0200

    my new module

---
+++ sergey_fedotov/src/main/java/session1/Task0.java
@@ -0,0 +1,10 @@
+package session1;
+
+/**
+ * Created by warit on 10.01.2015.
+ */
+public class Task0 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
