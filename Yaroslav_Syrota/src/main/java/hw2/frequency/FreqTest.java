168b53fac34028f5db20e1584ef34513443fc17d
CATCaterpiller <caterpiller@ex.ua>
2015-01-21 18:52:56 +0200

    AuthTest

---
+++ Yaroslav_Syrota/src/main/java/hw2/frequency/FreqTest.java
@@ -0,0 +1,11 @@
+package hw2.frequency;
+
+/**
+ * Created by Yaroslav_Syrota on 21.01.2015.
+ */
+public class FreqTest {
+    public static void main(String[] args) {
+        Freq freq = new Freq();
+
+    }
+}
