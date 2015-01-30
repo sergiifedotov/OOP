d335b975784da064aac868a828f271a7d3bb6c45
alexandr_lapada <alexlapada@gmail.com>
2015-01-31 00:55:37 +0200

    download Home Work 3 TextReader

--- alexandr_lapada/src/main/java/hw3/reader/TextReaderTest.java
+++ alexandr_lapada/src/main/java/hw3/reader/TextReaderTest.java
@@ -1,5 +1,8 @@
 package hw3.reader;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  Написать текстовый ридер с плавным движением текста вниз. Текст читается из файла. Стоп от ентер.
  public void read()
@@ -11,4 +14,35 @@ package hw3.reader;
  hw3.reader.TextReaderTest
  */
 public class TextReaderTest {
+    public static void main(String[] args) {
+        TextReader textReader = new TextReader();
+
+        Thread readEnter = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                int code;
+                try {
+                    while (-1 != (code = System.in.read())) {
+                        if (code == 10) {
+                            textReader.setWait(!textReader.getWait());
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        readEnter.start();
+
+        try {
+            textReader.read();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        readEnter.stop();
+    }
 }
+
