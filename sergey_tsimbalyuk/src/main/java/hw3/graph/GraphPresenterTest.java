41e04ff5d5e163625bd970bda60fbcf06692943b
sergey_tsimbalyuk <sergey.tsimbalyuk@gmail.com>
2015-02-01 07:23:21 +0200

    Auth

--- sergey_tsimbalyuk/src/main/java/hw3/graph/GraphPresenterTest.java
+++ sergey_tsimbalyuk/src/main/java/hw3/graph/GraphPresenterTest.java
@@ -1,21 +1,21 @@
 package hw3.graph;
 
-import org.junit.Before;
-import org.junit.Test;
+//import org.junit.Before;
+//import org.junit.Test;
 
 public class GraphPresenterTest {
-
-    GraphPresenter graph;
-
-    @Before
-    public void setUp() throws Exception {
-
-        graph = new GraphPresenter();
-
-    }
-
-    @Test
-    public void testPrint() throws Exception {
-        graph.print();
-    }
+//
+//    GraphPresenter graph;
+//
+//    @Before
+//    public void setUp() throws Exception {
+//
+//        graph = new GraphPresenter();
+//
+//    }
+//
+//    @Test
+//    public void testPrint() throws Exception {
+//        graph.print();
+//    }
 }
