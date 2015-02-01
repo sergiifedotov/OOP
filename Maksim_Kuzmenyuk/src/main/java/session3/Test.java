357f2296296f8f8cc143e98cab21d8752e9ed99c
maksimtest <mkn_ua@ukr.net>
2015-02-01 13:16:01 +0200

    My Other Commit

---
+++ Maksim_Kuzmenyuk/src/main/java/session3/Test.java
@@ -0,0 +1,14 @@
+package session3;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maksim_Kuzmenyuk
+ * Date: 31.01.15
+ * Time: 13:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
