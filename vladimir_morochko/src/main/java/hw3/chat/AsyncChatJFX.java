baa1679234491e293941c29361fdecbf66214e52
vladimir_morochko <vmorochko@gmail.com>
2015-02-03 19:08:56 +0200

    hw3.chat.AsyncChat

--- vladimir_morochko/src/main/java/hw3/chat/AsyncChatJFX.java
+++ vladimir_morochko/src/main/java/hw3/chat/AsyncChatJFX.java
@@ -2,15 +2,20 @@ package hw3.chat;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+
 import javafx.event.EventHandler;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
@@ -22,52 +27,22 @@ import java.net.Socket;
 
 public class AsyncChatJFX extends Application {
 
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        int receivePort = 30000;
-//        String address = "localhost";
-//
-//        AsyncChatJFX asyncChatConsole = new AsyncChatJFX(address, receivePort);
-//        asyncChatConsole.go();
-//
-//        int sendPort = 30001;
-//        Socket socket = null;
-//        PrintWriter printWriter = null;
-//
-//        try {
-//            socket = new ServerSocket(sendPort).accept();
-//            printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-//            System.out.println("opened port " + sendPort);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        while (true) {
-//            printWriter.println(scanner.nextLine());
-//            printWriter.flush();
-//        }
-//
-//    }
-
     ChatReceiver chatReceiver;
 
+    TextArea chatArea;
+    PrintWriter printWriter = null;
+    Socket sendSocket = null;
+
     public AsyncChatJFX() {
+        chatArea = new TextArea();
     }
 
-    public AsyncChatJFX(String address, int receivePort) {
-        chatReceiver = new ChatReceiver(address, receivePort);
-    }
-
-    public void go() {
-        chatReceiver.start();
-    }
 
     private class ChatReceiver implements Runnable {
         private Thread thread;
         String address;
         int port;
-        Socket socket;
+        Socket receiveSocket = null;
         BufferedReader bufferedReader;
 
         public ChatReceiver() {
@@ -90,51 +65,118 @@ public class AsyncChatJFX extends Application {
 
         @Override
         public void run() {
+
             try {
-                socket = new Socket(address, port);
-                bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                receiveSocket = new Socket(address, port);
+                bufferedReader = new BufferedReader(new InputStreamReader(receiveSocket.getInputStream()));
                 System.out.println("connected to port " + port + " on " + address);
                 while (!thread.isInterrupted()) {
-                    System.out.println(bufferedReader.readLine());
+
+                    String message = bufferedReader.readLine();
+                    if (message == null) {
+                        break;
+                    }
+                    System.out.println(message);
+                    chatArea.appendText("incoming: " + message);
+                    //TODO что здесь?
                 }
+
             } catch (IOException e) {
                 e.printStackTrace();
-             }
+            } finally {
+                if (receiveSocket != null) {
+                    try {
+                        receiveSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        TextField chatField = new TextField();
+        int receivePort = 30000;
+        String address = "localhost";
+        chatReceiver = new ChatReceiver(address, receivePort);
+        chatReceiver.start();
+
+        int sendPort = 30001;
+
+
+        try {
+            sendSocket = new ServerSocket(sendPort).accept();
+            printWriter = new PrintWriter(new OutputStreamWriter(sendSocket.getOutputStream()));
+            printWriter.println("hello server");
+            printWriter.flush();
+            System.out.println("opened port " + sendPort);
+        } catch (IOException e) {
+            e.printStackTrace();
+            if (sendSocket != null) {
+                try {
+                    sendSocket.close();
+                } catch (IOException e2) {
+                    e2.printStackTrace();
+                }
+            }
+        }
+
         TextField inputField = new TextField();
 
         Button btnSend = new Button("Send");
         btnSend.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-
+                printWriter.println(inputField.getText());
+                printWriter.flush();
+                chatArea.appendText(inputField.getText() + "\n");
+                inputField.setText("");
             }
         });
-        btnSend.setMinWidth(100);
 
         int sceneWidth = 600;
         int sceneHeight = 400;
+        int inputFieldHeight = sceneHeight / 10;
+
+        btnSend.setMinWidth(sceneWidth / 6);
+        btnSend.setMinHeight(inputFieldHeight);
+
         inputField.setMinWidth(sceneWidth - btnSend.getMinWidth());
+        inputField.setMinHeight(inputFieldHeight);
+        chatArea.setMinHeight(sceneHeight - inputFieldHeight);
+        chatArea.setWrapText(true);
+        chatArea.setEditable(false);
+
 
         GridPane gridPane = new GridPane();
-        gridPane.add(inputField, 0, 0);
         gridPane.add(btnSend, 1, 0);
+        gridPane.add(inputField, 0, 0);
 
         BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(chatField);
+        borderPane.setCenter(chatArea);
         borderPane.setBottom(gridPane);
 
         Scene scene = new Scene(borderPane, sceneWidth, sceneHeight);
         primaryStage.setScene(scene);
+        primaryStage.setTitle("AsyncChatJFX client");
+        primaryStage.setResizable(false);
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                System.out.println("closing...");
+                chatReceiver.interrupt();
+                if (sendSocket != null) {
+                    try {
+                        sendSocket.close();
+                    } catch (IOException e2) {
+                        e2.printStackTrace();
+                    }
+                }
+
+            }
+        });
         primaryStage.show();
-
     }
-
-
 }
