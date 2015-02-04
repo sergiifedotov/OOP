2370169043d2f8e0ab718372d06d724ae31c7e3d
alexandr_lapada <alexlapada@gmail.com>
2015-02-04 22:30:29 +0200

    download Home Work 4 asyncchat

--- alexandr_lapada/src/main/java/hw4/chat/Client.java
+++ alexandr_lapada/src/main/java/hw4/chat/Client.java
@@ -2,6 +2,7 @@ package hw4.chat;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 /**
@@ -10,36 +11,39 @@ import java.util.Scanner;
 public class Client {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        Socket socket = new Socket("127.0.0.1", 15000);
+        String dataIn = "";
+        Scanner in = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        try {
-            String dataIn = "";
-            Scanner in = new Scanner(System.in);
-            Socket socket = new Socket("127.0.0.1",1515);
-            PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        Thread clientWrite = new Thread(new Runnable() {
+            String dataOut = "";
 
-            Thread clientWrite = new Thread(new Runnable(){
-                String dataOut = "";
-                @Override
-                public void run() {
-                    while(true) {
-                        dataOut = in.next();
-                        pw.println(dataOut);
-                        pw.flush();
-                    }
+            @Override
+            public void run() {
+                while (true) {
+                    dataOut = in.next();
+                    pw.println(dataOut);
+                    pw.flush();
                 }
-            });
-            clientWrite.start();
+            }
+        });
+        clientWrite.start();
 
-            while(true){
+
+            while (true) {
+
                 dataIn = br.readLine();
+                if (dataIn == null){
+                    System.out.println("connection lost");
+                    System.exit(1);
+                    break;
+                }
                 System.out.println(dataIn);
-               }
+            }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
 
