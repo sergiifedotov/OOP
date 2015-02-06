f341de505ed6780fc9513d4a3ad1984063bf4d33
alexandr_lapada <alexlapada@gmail.com>
2015-02-06 14:42:10 +0200

    download Home Work 4 filefinder

--- alexandr_lapada/src/main/java/hw4/parallel/FileFinder.java
+++ alexandr_lapada/src/main/java/hw4/parallel/FileFinder.java
@@ -1,7 +1,35 @@
 package hw4.parallel;
 
+import java.io.File;
+import java.util.Scanner;
+
 /**
  * Created by sanya on 02.02.2015.
  */
-public class FileFinder {
+public class FileFinder extends Thread {
+    @Override
+    public void run() {
+        Scanner input = new Scanner(System.in);
+        File f;
+        String path, file;
+        boolean result = false;
+        System.out.println("Enter the required path for search: ");
+        path = input.nextLine();
+        f = new File(path + ".");
+        //System.out.println(f.getAbsolutePath());
+        //System.out.println(f.getPath());
+        System.out.println("Enter the name of required file: ");
+        file = input.nextLine();
+        File[] files = f.listFiles();
+        for (int i = 0;i < files.length; i++) {
+            if (files[i].toString().equals(path + ".\\" + file) && files[i].isFile()) {
+                result = true;
+                break;
+            }
+        }
+        if (result == true)
+
+            System.out.println("File located in the folder");
+        else System.out.println("File was not found ");
+    }
 }
