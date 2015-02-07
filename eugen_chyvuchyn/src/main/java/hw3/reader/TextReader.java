330f347a6321bdbd5e05a717791b2cc925c446e7
eugen_chyvuchyn <chuvendoil@gmail.com>
2015-02-07 15:20:29 +0200

    upd hw

---
+++ eugen_chyvuchyn/src/main/java/hw3/reader/TextReader.java
@@ -0,0 +1,77 @@
+package hw3.chat.reader;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by Chuvychin on 07.02.2015.
+ */
+public class TextReader {
+    public static void main(String[] args) throws IOException {
+        TextReader tr = new TextReader("D:/txt/Firm.txt/",800);
+        tr.read();
+
+
+    }
+    private String fileName;
+    private long timer;
+    BufferedReader br;
+    List<String> fileContent = new ArrayList<String>();
+    Output out = new Output();
+    Scanner scan;
+
+
+    public TextReader(String fileName, int timer){
+        this.fileName = fileName;
+        this.timer = timer;
+    }
+
+    public void read() {
+        try {
+            br = new BufferedReader( new InputStreamReader(new FileInputStream(fileName)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String str;
+        try {
+            while( (str = br.readLine()) !=null){
+                fileContent.add(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        out.start();
+
+
+
+
+        try {
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    class Output extends Thread{
+
+        @Override
+        public void run(){
+
+            for (int i=0;i<fileContent.size();i++){
+                try {
+                    Thread.sleep(timer);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(fileContent.get(i));
+
+            }
+        }
+    }
+}
