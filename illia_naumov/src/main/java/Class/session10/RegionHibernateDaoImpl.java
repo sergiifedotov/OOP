03703b03c7dd6db5ed97f0ca8d74dad9d01c496a
illia-maks <email@gmail.com>
2015-02-08 16:22:58 +0200

    commit hibernate session 10

---
+++ illia_naumov/src/main/java/Class/session10/RegionHibernateDaoImpl.java
@@ -0,0 +1,109 @@
+package Class.session10;
+
+import Class.session9.Region;
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: al1
+ * Date: 20.09.14
+ */
+public class RegionHibernateDaoImpl implements RegionDao {
+    private static Logger log = Logger.getLogger(RegionHibernateDaoImpl.class);
+    private SessionFactory factory;
+
+    public RegionHibernateDaoImpl(SessionFactory factory) {
+        this.factory = factory;
+    }
+
+    @Override
+    public Long create(Region region) {
+        Session session = null;
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+            Long id = (Long)session.save(region);
+            session.getTransaction().commit();
+            return id;
+        } catch (HibernateException e) {
+            log.error("Open session failed", e);
+            session.getTransaction().rollback();
+        } finally {
+            if(session != null) {
+                session.close();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Region read(Long id) {
+        Session session = null;
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+            Region reg = (Region) session.get(Region.class, id);
+            session.getTransaction().commit();
+            return reg;
+        } catch (HibernateException e) {
+            log.error("Open session failed", e);
+            session.getTransaction().rollback();
+        } finally {
+            if(session != null) {
+                session.close();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void update(Region region) {
+        Session session = null;
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+            session.update(region);
+            session.getTransaction().commit();
+
+        } catch (HibernateException e) {
+            log.error("Open session failed", e);
+            session.getTransaction().rollback();
+        } finally {
+            if(session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public void delete(Region region) {
+        Session session = null;
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+            Region reg = read(region.getId());
+            session.delete(reg);
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            log.error("Open session failed", e);
+            session.getTransaction().rollback();
+        } finally {
+            if(session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Region> findAll() {
+        // NO implementation
+        return null;
+    }
+
+
+}
