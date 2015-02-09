e7ddd8a49c46718316106c8dc84fbad846f8c6e8
sergey_tsimbalyuk <sergey.tsimbalyuk@gmail.com>
2015-02-09 18:07:39 +0200

    Global Upd

--- sergey_tsimbalyuk/src/main/java/session05/ThreetonTest.java
+++ sergey_tsimbalyuk/src/main/java/session05/ThreetonTest.java
@@ -1,39 +1,39 @@
 package session05;
 
-import org.junit.Before;
-import org.junit.Test;
+//import org.junit.Before;
+//import org.junit.Test;
 
-import java.io.IOException;
+//import java.io.IOException;
 
-import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.assertTrue;
 
 /**
  * Created by tsv on 24.01.15.
  */
 public class ThreetonTest {
-    //логика
-    private ThreetonLazy instance;
-
-    @Before
-    public void setUp() {
-        instance = ThreetonLazy.getInstanceOne();
-    }
-
-//    @Ignore //what????
-    @Test(expected = IOException.class)
-    public void testNull() {
-        ThreetonLazy threetonLazy1 = ThreetonLazy.getInstanceOne();
-        ThreetonLazy threetonLazy2 = ThreetonLazy.getInstanceTwo();
-        ThreetonLazy threetonLazy3 = ThreetonLazy.getInstanceThree();
-
-        assertTrue(threetonLazy1 != threetonLazy2 && threetonLazy2 != threetonLazy3 && threetonLazy3 != threetonLazy1);
-//        {
-//            System.out.println("Test ok!");
-//        if (threetonLazy1 != threetonLazy2 && threetonLazy2 != threetonLazy3 && threetonLazy3 != threetonLazy1) {
-//            System.out.println("Test ok!");
-//        } else {
-//            System.out.println("Failed");
+//    //логика
+//    private ThreetonLazy instance;
+//
+//    @Before
+//    public void setUp() {
+//        instance = ThreetonLazy.getInstanceOne();
+//    }
+//
+////    @Ignore //what????
+//    @Test(expected = IOException.class)
+//    public void testNull() {
+//        ThreetonLazy threetonLazy1 = ThreetonLazy.getInstanceOne();
+//        ThreetonLazy threetonLazy2 = ThreetonLazy.getInstanceTwo();
+//        ThreetonLazy threetonLazy3 = ThreetonLazy.getInstanceThree();
+//
+//        assertTrue(threetonLazy1 != threetonLazy2 && threetonLazy2 != threetonLazy3 && threetonLazy3 != threetonLazy1);
+////        {
+////            System.out.println("Test ok!");
+////        if (threetonLazy1 != threetonLazy2 && threetonLazy2 != threetonLazy3 && threetonLazy3 != threetonLazy1) {
+////            System.out.println("Test ok!");
+////        } else {
+////            System.out.println("Failed");
 //        }
 
-        }
+//        }
 }
--- sergey_tsimbalyuk/src/main/java/session10/dentist/utils/HibernateUtil.java
+++ sergey_tsimbalyuk/src/main/java/session10/dentist/utils/HibernateUtil.java
@@ -11,7 +11,7 @@ import java.util.Locale;
  * User: al1
  * Date: 5/12/13
  */
-public class HibernateUtil {
+public class HibernateUtil {/*
     private static final SessionFactory sessionFactory = buildSessionFactory();
     private static SessionFactory buildSessionFactory() {
         try {
@@ -29,5 +29,5 @@ public class HibernateUtil {
 
     public static Session getSession() {
         return sessionFactory.openSession();
-    }
+    }*/
 }
