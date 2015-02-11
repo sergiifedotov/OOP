6df353aeaa12669cef28fa51c484b23b2f733305
vladimir_morochko <vmorochko@gmail.com>
2015-02-11 12:18:19 +0200

    hw6.notes.domain.Notebook hw6.notes.dao.NotebookDao hw6.notes.dao.NotebookDaoImpl

---
+++ vladimir_morochko/src/main/java/hw6/notes/dao/NotebookDao.java
@@ -0,0 +1,31 @@
+package hw6.notes.dao;
+
+import hw6.notes.domain.Notebook;
+
+import java.util.List;
+
+/**
+ * Created by vladimir on 11.02.2015.
+ *
+ *  Создать DAO для таблицы ноутбуки
+ Таблица ноутбуки имеет следующую структуру
+ (id, serial, vendor, model, manufacture date, price)
+ domain
+ hw6.notes.domain.Notebook
+ dao
+ hw6.notes.dao.NotebookDao
+ Long create(Notebook ntb)
+ Notebook read(Long ig)
+ boolean update(Notebook ntb)
+ boolean delete(Notebook ntb)
+ List<Notebook> findAll()
+ hw6.notes.dao.NotebookDaoImpl
+ */
+
+public interface NotebookDao {
+    Long create(Notebook notebook);
+    Notebook read(Long id);
+    boolean update(Notebook notebook);
+    boolean delete(Notebook notebook);
+    List<Notebook> findAll();
+}
