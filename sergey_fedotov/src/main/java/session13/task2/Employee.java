7a44d3591813d727e3fc99a620e54ff517b3c951
sergey_fedotov <sergey_fedotov@sergey_fedotov>
2015-02-21 23:40:49 +0200

    update session task2

---
+++ sergey_fedotov/src/main/java/session13/task2/Employee.java
@@ -0,0 +1,79 @@
+package session13.task2;
+
+import javax.persistence.*;
+
+/**
+ */
+
+@Entity
+@Table(name = "EMPLOYEES")
+public class Employee {
+    @SequenceGenerator(name = "sequence", sequenceName = "SEQ_EMPLOYEES_ID",
+            allocationSize = 1, initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence")
+    @Id
+    @Column (name = "EMPLOYEE_ID")
+    private Long id;
+
+    @Column (name = "FIRST_NAME")
+    private String first_name;
+
+    @Column (name = "LAST_NAME")
+    private String last_name;
+
+    @Column (name = "SALARY")
+    private Double salary;
+
+    public Employee() {
+    }
+
+    public Employee(String first_name, String last_name, Double salary) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.salary = salary;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ sergey_fedotov/src/main/java/session13/task2/HiberConnect.java
@@ -0,0 +1,34 @@
+package session13.task2;
+
+import org.apache.log4j.Logger;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Locale;
+
+/**
+ *
+ */
+public class HiberConnect {
+    private static Logger log = Logger.getLogger(HiberConnect.class);
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
+        Configuration cfg = new Configuration().configure("session13/task2/context.xml");
+        StandardServiceRegistryBuilder sb = new StandardServiceRegistryBuilder();
+        sb.applySettings(cfg.getProperties());
+        StandardServiceRegistry standardServiceRegistry = sb.build();
+        SessionFactory factory = cfg.buildSessionFactory(standardServiceRegistry);
+        log.info("Reference to SessionFactory " + factory);
+
+        //EmployeeDaoImpl employeeDaoImpl = new EmployeeDaoImpl(factory);
+        //System.out.println(employeeDaoImpl.findAll().toString());
+
+        if (factory != null) {
+            factory.close();
+        }
+    }
+}
+
