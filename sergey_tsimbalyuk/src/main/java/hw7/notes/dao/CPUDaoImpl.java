0c7b183e81cf55a04dfe25b5ab84f9ffdc228867
sergey_tsimbalyuk <sergey.tsimbalyuk@gmail.com>
2015-02-27 18:34:07 +0200

    hw7.notes

---
+++ sergey_tsimbalyuk/src/main/java/hw7/notes/dao/CPUDaoImpl.java
@@ -0,0 +1,50 @@
+package hw7.notes.dao;
+
+import hw7.notes.domain.CPU;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by Sergey Tsimbalyuk
+ * on 27.02.15
+ */
+@Repository
+@Transactional
+public class CPUDaoImpl implements CPUDao {
+    @Qualifier("mySessionFactory")
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public Long create(CPU cpu) {
+        return (Long) sessionFactory.getCurrentSession().save(cpu);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public CPU read(Long id) {
+        return (CPU) sessionFactory.getCurrentSession().get(CPU.class, id);
+    }
+
+    @Override
+    public boolean update(CPU cpu) {
+        sessionFactory.getCurrentSession().update(cpu);
+        return true;
+    }
+
+    @Override
+    public boolean delete(CPU cpu) {
+        sessionFactory.getCurrentSession().delete(cpu);
+        return true;
+    }
+
+    @Override
+    public List<CPU> findAll() {
+        return (List<CPU>) sessionFactory.getCurrentSession().createCriteria(CPU.class).list();
+    }
+}
