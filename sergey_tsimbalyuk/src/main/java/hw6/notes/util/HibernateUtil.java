a13a2cd15d68aeace7e7b535dbed185ae1e24d17
sergey_tsimbalyuk <sergey.tsimbalyuk@gmail.com>
2015-02-27 14:05:47 +0200

    hw6.notes.upd

---
+++ sergey_tsimbalyuk/src/main/java/hw6/notes/util/HibernateUtil.java
@@ -0,0 +1,31 @@
+package hw6.notes.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: al1
+ * Date: 5/12/13
+ */
+public class HibernateUtil {
+/*
+del
+ */
+/*
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Locale.setDefault(Locale.ENGLISH);
+            return new AnnotationConfiguration().configure().buildSessionFactory();
+        }
+        catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static Session getSession() {
+        return sessionFactory.openSession();
+    }*/
+}
