c5778dc8bcffecbe36e80d13e2ed30f7d7c32809
sergey_tsimbalyuk <sergey.tsimbalyuk@gmail.com>
2015-03-01 17:25:47 +0200

    sergey_tsimbalyuk_web session16

---
+++ sergey_tsimbalyuk_web/src/main/java/controller/LoginServer.java
@@ -0,0 +1,63 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: al1
+ * Date: 28.02.15
+ */
+@WebServlet("/lsrv")
+public class LoginServer extends HttpServlet {
+
+    Map<String, String> list = new HashMap<>();
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        list.put("name1", "pass1");
+        list.put("name2", "pass2");
+        list.put("name3", "pass3");
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        String login = parameterMap.get("login")[0];
+        String password = parameterMap.get("password")[0];
+        try {
+            if (Auth(login, password)) {
+//                request.setAttribute("key", new String("Wrong LOGIN / PASSWORD!"));
+//                request.setAttribute("list", list);
+                request.getRequestDispatcher("passok.jsp").forward(request, response);
+            } else {
+
+//                request.getRequestDispatcher("wronglogpwd.jsp").forward(request, response);
+                request.setAttribute("key", new String("Wrong LOGIN / PASSWORD!"));
+                request.getRequestDispatcher("index.jsp").forward(request, response);
+
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean Auth(String login, String password) {
+        if (password.equals(list.get(login))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+//    parameterMap = request.getParameterMap();
+        /*Map<String, String[]> parameterMap=request.getParameterMap();
+        response.getWriter().print("Hello, " + parameterMap.get("login"));*/
+//            request.getRequestDispatcher("index.jsp").forward(request, response);
+        
---
+++ sergey_tsimbalyuk_web/src/main/webapp/index_old.jsp
@@ -0,0 +1,52 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: al1
+  Date: 05.10.14
+--%>
+<%--<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hellox`</title>
+</head>
+
+<body>
+<h3> Hello Web </h3>
+
+<p> Paragraph </p>
+text</br>
+<a href="bye.jsp">Link</a>
+<%--<img src="" />--%>
+</br>
+<img class="media-image" src="https://pbs.twimg.com/media/B-wded8VEAAgJpW.jpg:large" data-height="565" data-width="414"
+     style="width: 298px; height: 405px; margin-top: 0px; margin-bottom: 0px;">
+
+<ul>
+
+</ul>
+<ol>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+</ol>
+<table border="1">
+    <%--тег border использовать не стоит, пользуются блочно версткой с помощью CSS--%>
+    <thead></thead>
+    <tbody>
+    <tr>
+        <td>qq</td>
+        <td>qw</td>
+        <td>qe</td>
+        <td>qr</td>
+    </tr>
+    <tr>
+        <td>wq</td>
+        <td>ww</td>
+        <td>we</td>
+        <td>wr</td>
+    </tr>
+    </tbody>
+</table>
+</body>
+</html>
