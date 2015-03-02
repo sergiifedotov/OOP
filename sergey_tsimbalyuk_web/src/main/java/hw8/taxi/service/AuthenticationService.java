d2bf7d62cfa3f392754dbc2020ab7ff0dbb0d8c6
sergey_tsimbalyuk <sergey.tsimbalyuk@gmail.com>
2015-03-02 18:32:25 +0200

    hw8 template

---
+++ sergey_tsimbalyuk_web/src/main/java/hw8/taxi/service/AuthenticationService.java
@@ -0,0 +1,18 @@
+package hw8.taxi.service;
+
+
+import hw8.taxi.exception.AuthenticationException;
+
+/**
+ * Created by Sergey Tsimbalyuk
+ * on 02.03.15
+ */
+public class AuthenticationService {
+    boolean authenticate(String login, String pass) throws AuthenticationException {
+        return false;
+    }
+    boolean register(String login, String id, String pass) throws AuthenticationException {
+
+        return false;
+    }
+}
---
+++ sergey_tsimbalyuk_web/src/main/java/hw8/taxi/service/OrderServiceImpl.java
@@ -0,0 +1,8 @@
+package hw8.taxi.service;
+
+/**
+ * Created by Sergey Tsimbalyuk
+ * on 02.03.15
+ */
+public class OrderServiceImpl {
+}
