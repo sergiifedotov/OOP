f998c76cd501e255890b068f38012aed557ae1ed
sergey_tsimbalyuk <sergey.tsimbalyuk@gmail.com>
2015-03-03 20:55:49 +0200

    hw7 NotebookService upd

--- sergey_tsimbalyuk/src/main/java/hw7/notes/service/NotebookService.java
+++ sergey_tsimbalyuk/src/main/java/hw7/notes/service/NotebookService.java
@@ -1,5 +1,9 @@
 package hw7.notes.service;
 
+import hw7.notes.domain.Notebook;
+
+import java.util.List;
+
 /**
  * Created by Sergey Tsimbalyuk
  * on 27.02.15
@@ -7,4 +11,8 @@ package hw7.notes.service;
 public interface NotebookService {
     Long receive(int notebookId, int amount, double price);
     Long sale(int storeId, int amount);
+
+    List<Notebook> getNotebooksFromStore();
+    List<Notebook> getNotebooksStorePresent();
+    List<Notebook> getSalesByDays();
 }
