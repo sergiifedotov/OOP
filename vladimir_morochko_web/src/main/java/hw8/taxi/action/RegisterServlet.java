f3db401d73762f3741882bc3407deae50879269b
vladimir_morochko <vmorochko@gmail.com>
2015-03-03 08:09:01 +0200

    hw8.taxi.action draft

--- vladimir_morochko_web/src/main/java/hw8/taxi/action/RegisterServlet.java
+++ vladimir_morochko_web/src/main/java/hw8/taxi/action/RegisterServlet.java
@@ -1,7 +1,17 @@
 package hw8.taxi.action;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by vladimir on 03.03.2015.
  */
-public class RegisterServlet {
+public class RegisterServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
