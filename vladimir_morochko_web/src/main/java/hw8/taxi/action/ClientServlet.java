b7ee44d3a94f4382f454dd1c07a43427561637e8
vladimir_morochko <vmorochko@gmail.com>
2015-03-03 08:12:51 +0200

    hw8.taxi.action draft 2

--- vladimir_morochko_web/src/main/java/hw8/taxi/action/ClientServlet.java
+++ vladimir_morochko_web/src/main/java/hw8/taxi/action/ClientServlet.java
@@ -1,6 +1,7 @@
 package hw8.taxi.action;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +10,7 @@ import java.io.IOException;
 /**
  * Created by vladimir on 03.03.2015.
  */
+@WebServlet("/ClientServlet")
 public class ClientServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
