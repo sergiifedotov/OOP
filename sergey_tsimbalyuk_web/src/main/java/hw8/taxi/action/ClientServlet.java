a601ffb90cf645a77036e8da20bc54ba41fb0832
sergey_tsimbalyuk <sergey.tsimbalyuk@gmail.com>
2015-03-03 18:10:44 +0200

    web_hw8 ClientServlet upd

--- sergey_tsimbalyuk_web/src/main/java/hw8/taxi/action/ClientServlet.java
+++ sergey_tsimbalyuk_web/src/main/java/hw8/taxi/action/ClientServlet.java
@@ -1,7 +1,11 @@
 package hw8.taxi.action;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by Sergey Tsimbalyuk
@@ -9,4 +13,8 @@ import javax.servlet.http.HttpServlet;
  */
 @WebServlet("/client")
 public class ClientServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("registerClient.jsp").forward(req, resp);
+    }
 }
