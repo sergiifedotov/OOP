b35494a56c5505cae475d811aa5fff6b601cb172
sergey_fedotov <sergey_fedotov@sergey_fedotov>
2015-03-05 02:29:39 +0200

    update hw8 2

--- sergey_fedotov_web/src/main/java/hw8/taxi/action/RegisterServlet.java
+++ sergey_fedotov_web/src/main/java/hw8/taxi/action/RegisterServlet.java
@@ -19,7 +19,7 @@ import java.util.Map;
 public class RegisterServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
     }
 
