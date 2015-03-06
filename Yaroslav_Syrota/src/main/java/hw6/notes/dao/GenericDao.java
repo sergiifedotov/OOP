a311fa4068541f800f119fd4d9b0cf92c44ea275
Yaroslav_Syrota <caterpiller@ex.ua>
2015-03-06 09:44:57 +0200

    web

--- Yaroslav_Syrota/src/main/java/hw6/notes/dao/GenericDao.java
+++ Yaroslav_Syrota/src/main/java/hw6/notes/dao/GenericDao.java
@@ -8,16 +8,16 @@ import java.util.List;
  * User: al1
  * Date: 8/18/13
  */
-public interface GenericDao<T, PK extends Serializable> {
-    PK create(T persistentObject);
-
-    T get(PK id);
-
-    List<T> getAll();
-
-    void update(T persistentObject);
-
-    void createOrUpdate(T persistentObject);
-
-    void delete(T persistentObject);
-}
+//public interface GenericDao<T, PK extends Serializable> {
+//    PK create(T persistentObject);
+//
+//    T get(PK id);
+//
+//    List<T> getAll();
+//
+//    void update(T persistentObject);
+//
+//    void createOrUpdate(T persistentObject);
+//
+//    void delete(T persistentObject);
+//}
