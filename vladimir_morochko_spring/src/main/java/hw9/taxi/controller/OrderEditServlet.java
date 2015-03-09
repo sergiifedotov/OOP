73c7b25e04542ea19c9015a8b6d3e0f4fd73b7f2
vladimir_morochko <vmorochko@gmail.com>
2015-03-09 11:16:16 +0200

    hw9.taxi draft2

--- vladimir_morochko_spring/src/main/java/hw9/taxi/controller/OrderEditServlet.java
+++ vladimir_morochko_spring/src/main/java/hw9/taxi/controller/OrderEditServlet.java
@@ -1,7 +1,18 @@
 package hw9.taxi.controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by vladimir on 09.03.2015.
  */
-public class OrderEditServlet {
+public class OrderEditServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
 }
