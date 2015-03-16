8dd6f599c2af668be43fd253791f8aa082b3b93d
vladimir_morochko <vmorochko@gmail.com>
2015-03-16 08:20:54 +0200

    files restore (illia-maks, we need to talk!)

--- vladimir_morochko/src/main/java/hw7/springnotes/dao/SalesDao.java
+++ vladimir_morochko/src/main/java/hw7/springnotes/dao/SalesDao.java
@@ -7,15 +7,14 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by vladimir on 17.02.2015.
+ * Created by vladimir on 23.02.2015.
  */
 public interface SalesDao {
     Long create(Sales sales);
-    Sales read(Long ig);
+    Sales read(Long id);
     boolean update(Sales sales);
     boolean delete(Sales sales);
     List<Sales> findAll();
-    Map<Notebook,Integer> getSalesByDays();
 
-    void close();
+    List<Sales> getSalesByDays();
 }
