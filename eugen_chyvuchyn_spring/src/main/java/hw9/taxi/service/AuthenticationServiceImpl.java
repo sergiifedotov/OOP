496781df999219061dfa40d536a98b7ac1031eae
eugen_chyvuchyn <chuvendoil@gmail.com>
2015-03-21 15:30:13 +0200

    upd pack

--- eugen_chyvuchyn_spring/src/main/java/hw9/taxi/service/AuthenticationServiceImpl.java
+++ eugen_chyvuchyn_spring/src/main/java/hw9/taxi/service/AuthenticationServiceImpl.java
@@ -1,13 +1,33 @@
 package hw9.taxi.service;
 
+import hw9.taxi.dao.UserDao;
 import hw9.taxi.exception.AuthenticationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by Chuvychin on 13.03.2015.
  */
+@Component
+@Transactional
 public class AuthenticationServiceImpl implements AuthenticationService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public AuthenticationServiceImpl() {
+    }
+
+
     @Override
+    @Transactional (readOnly = true)
+
     public boolean authenticate(String login, String pass) throws AuthenticationException {
-        return false;
+        if (userDao.readByLogin(login).getPass() == pass){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
